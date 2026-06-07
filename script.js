ac0210773b669adf5735ae6a0e618218120567d4
khushalii9999 <patelkhushali064@gmail.com>
2026-06-07 16:10:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+function openLetter(){
+
+    document.getElementById("page1").style.display="none";
+
+    document.getElementById("page2").style.display="flex";
+
+}
+function nextPage3(){
+
+    document.getElementById("page2").style.display="none";
+
+    document.getElementById("page3").style.display="flex";
+
+}
+function nextPage4(){
+
+    document.getElementById("page3").style.display="none";
+
+    document.getElementById("page4").style.display="flex";
+}
+function nextPage5(){
+
+    document.getElementById("page4").style.display="none";
+
+    document.getElementById("page5").style.display="flex";
+
+}
+function nextPage6(){
+
+    document.getElementById("page5").style.display="none";
+
+    document.getElementById("page6").style.display="flex";
+
+}
